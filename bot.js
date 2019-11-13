691efb04f005270e0aec9afbfa2ed8df7c697239
anasanwar2 <57671285+anasanwar2@users.noreply.github.com>
2019-11-13 15:43:35 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("643813432772132867")
+setInterval(function() {
+channel.send(`Rj Anas the top in the humenbengis and fly with ground feby here bye bye bye beby Rj Anas the top in the humenbengis and fly with ground feby here bye bye bye beby Rj Anas the top in the humenbengis and fly with ground feby here bye bye bye beby `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
